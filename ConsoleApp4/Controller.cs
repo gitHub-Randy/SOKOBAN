61cef28d3a57af566d6daf6cc96e7ca360d607e7
randyAnft <ranft@avans.nl>
2018-10-07 14:53:20 +0200

    fasdfsadf
    dfasfsa

--- ConsoleApp4/Controller.cs
+++ ConsoleApp4/Controller.cs
@@ -15,19 +15,46 @@ namespace ConsoleApp4
         private GameView gameView;
         public Controller()
         {
+
             _dh = new Doolhof();
-            _parser = new Parser(this);
+           
             _inputView = new InputView();
-            _inputView.ShowMenu();
-            keyInputEvent();
+            PlayGame();
 
-            while (!CheckWin())
+
+
+
+
+        }
+
+        private void PlayGame()
+        {
+            while (true)
             {
-                KeyInputEventGame();
+                _parser = new Parser(this);
+                _inputView.ShowMenu();
+                keyInputEvent();
+                while (!CheckWin())
+                {
+                    KeyInputEventGame();
+                }
+                Console.Clear();
+
             }
+        }
 
 
 
+        public void makeLevel(int levelNew)
+        {
+            level = levelNew;
+            Console.Clear();
+            _parser.ParseLevel(levelNew);
+            _board = _parser.Object;
+            numOfRows = _parser.NumberOfRows;
+            lengthOfRows = _parser.LengthOfRows;
+            gameView = new GameView(_board, numOfRows, lengthOfRows);
+            return;
         }
 
         private void keyInputEvent()
@@ -38,87 +65,151 @@ namespace ConsoleApp4
                 choice = Console.ReadKey();
                 Console.WriteLine("");
                 Console.WriteLine("> kies een doolhof ( 1 - 4), s = stop");
-                if (choice.Key == ConsoleKey.D1)
+
+                switch (choice.Key)
                 {
-                    Console.Clear();
-                    level = 1;
-                    _parser.ParseLevel(1);
-                    _board = _parser.Object;
-                    numOfRows = _parser.NumberOfRows;
-                    lengthOfRows = _parser.LengthOfRows;
-                    gameView = new GameView(_board, numOfRows, lengthOfRows);
-                    return;
+                    case ConsoleKey.D1:
+                        makeLevel(1);
+                        return;
+                    case ConsoleKey.D2:
+                        makeLevel(2);
+                        return;
+                    case ConsoleKey.D3:
+                        makeLevel(3);
+                        return;
+                    case ConsoleKey.D4:
+                        makeLevel(4);
+                        return;
+                    case ConsoleKey.D5:
+                        makeLevel(5);
+                        return;
+                    case ConsoleKey.D6:
+                        makeLevel(6);
+                        return;
+                    case ConsoleKey.S:
+                        Environment.Exit(0);
+                        return;
+                    default:
+                        Console.WriteLine("Geef juiste input");
+                        break;
                 }
-                if (choice.Key == ConsoleKey.D2)
-                {
-                    Console.Clear();
-                    level = 2;
-                    _parser.ParseLevel(2);
-                    _board = _parser.Object;
-                    numOfRows = _parser.NumberOfRows;
-                    lengthOfRows = _parser.LengthOfRows;
-                    gameView = new GameView(_board, numOfRows, lengthOfRows);
-                    return;
-                }
-                if (choice.Key == ConsoleKey.D3)
-                {
-                    Console.Clear();
-                    level = 3;
-                    _parser.ParseLevel(3);
-                    _board = _parser.Object;
-                    numOfRows = _parser.NumberOfRows;
-                    lengthOfRows = _parser.LengthOfRows;
-                    gameView = new GameView(_board, numOfRows, lengthOfRows);
-                    return;
-                }
-                if (choice.Key == ConsoleKey.D4)
-                {
-                    Console.Clear();
-                    level = 4;
-                    _parser.ParseLevel(4);
-                    _board = _parser.Object;
-                    numOfRows = _parser.NumberOfRows;
-                    lengthOfRows = _parser.LengthOfRows;
-                    gameView = new GameView(_board, numOfRows, lengthOfRows);
-                    return;
-                }
-                if (choice.Key == ConsoleKey.D5)
-                {
-                    Console.Clear();
-                    level = 5;
-                    _parser.ParseLevel(5);
-                    _board = _parser.Object;
-                    numOfRows = _parser.NumberOfRows;
-                    lengthOfRows = _parser.LengthOfRows;
-                    gameView = new GameView(_board, numOfRows, lengthOfRows);
-                    return;
-                }
-                if (choice.Key == ConsoleKey.D6)
-                {
-                    Console.Clear();
-                    level = 6;
-                    _parser.ParseLevel(6);
-                    _board = _parser.Object;
-                    numOfRows = _parser.NumberOfRows;
-                    lengthOfRows = _parser.LengthOfRows;
-                    gameView = new GameView(_board, numOfRows, lengthOfRows);
-                    return;
-                }
-                if (choice.Key == ConsoleKey.S)
-                {
-                    Environment.Exit(0);
-                }
-                Console.WriteLine("Geef juiste input");
-                continue;
+
             }
-            
+
+        }
+
+
+        private void MoveWest(int i, int j)
+        {
+            if (_board[i, j].WestField.Symbol != '#' && _board[i, j].WestField.Symbol != 'o' && _board[i, j].WestField.Symbol != '0')
+            {
+
+                _board[i, j].Object.Move(_board[i, j].WestField, _board[i, j], false);
+                MoveSleeper();
+                Console.Clear();
+                gameView.Board = this._board;
+                gameView.PrintObjects();
+
+                return;
+            }
+            else if (_board[i, j].WestField.Symbol == 'o' || _board[i, j].WestField.Symbol == '0')
+            {
+
+
+                _board[i, j].WestField.Object.Move(_board[i, j].WestField.WestField, _board[i, j].WestField, false);
+                _board[i, j].Object.Move(_board[i, j].WestField, _board[i, j], false);
+                MoveSleeper();
+                Console.Clear();
+                gameView.Board = this._board;
+                gameView.PrintObjects();
+                return;
+
+            }
+
+
+        }
+
+        private void MoveEast(int i, int j)
+        {
+            if (_board[i, j].EastField.Symbol != '#' && _board[i, j].EastField.Symbol != 'o' && _board[i, j].EastField.Symbol != '0')
+            {
+
+                _board[i, j].Object.Move(_board[i, j].EastField, _board[i, j], false);
+                MoveSleeper();
+                Console.Clear();
+                gameView.Board = this._board;
+                gameView.PrintObjects();
+                return;
+            }
+            else if (_board[i, j].EastField.Symbol == 'o' || _board[i, j].EastField.Symbol == '0')
+            {
+
+                _board[i, j].EastField.Object.Move(_board[i, j].EastField.EastField, _board[i, j].EastField, false);
+                _board[i, j].Object.Move(_board[i, j].EastField, _board[i, j], false);
+                MoveSleeper();
+                Console.Clear();
+                gameView.Board = this._board;
+                gameView.PrintObjects();
+                return;
+            }
+
+
+        }
+
+        private void MoveNorth(int i, int j)
+        {
+            if (_board[i, j].NorthField.Symbol != '#' && _board[i, j].NorthField.Symbol != 'o' && _board[i, j].NorthField.Symbol != '0')
+            {
+                _board[i, j].Object.Move(_board[i, j].NorthField, _board[i, j], false);
+                MoveSleeper();
+                Console.Clear();
+                gameView.Board = this._board;
+                gameView.PrintObjects();
+                return;
+            }
+            else if (_board[i, j].NorthField.Symbol == 'o' || _board[i, j].NorthField.Symbol == '0')
+            {
+
+                _board[i, j].NorthField.Object.Move(_board[i, j].NorthField.NorthField, _board[i, j].NorthField, false);
+                _board[i, j].Object.Move(_board[i, j].NorthField, _board[i, j], false);
+                MoveSleeper();
+                Console.Clear();
+                gameView.Board = this._board;
+                gameView.PrintObjects();
+                return;
+
+            }
+
+        }
+
+        private void MoveSouth(int i, int j)
+        {
+            if (_board[i, j].SouthField.Symbol != '#' && _board[i, j].SouthField.Symbol != 'o' && _board[i, j].SouthField.Symbol != '0')
+            {
+                _board[i, j].Object.Move(_board[i, j].SouthField, _board[i, j], false);
+                MoveSleeper();
+                Console.Clear();
+                gameView.Board = this._board;
+                gameView.PrintObjects();
+                return;
+            }
+            else if (_board[i, j].SouthField.Symbol == 'o' || _board[i, j].SouthField.Symbol == '0')
+            {
+                _board[i, j].SouthField.Object.Move(_board[i, j].SouthField.SouthField, _board[i, j].SouthField, false);
+                _board[i, j].Object.Move(_board[i, j].SouthField, _board[i, j], false);
+                MoveSleeper();
+                Console.Clear();
+                gameView.Board = this._board;
+                gameView.PrintObjects();
+                return;
+            }
         }
 
         private void KeyInputEventGame()
         {
             ConsoleKey choice;
 
-            
+
             for (int i = 0; i < numOfRows; i++)
             {
                 for (int j = 0; j < lengthOfRows; j++)
@@ -131,108 +222,22 @@ namespace ConsoleApp4
                             switch (choice)
                             {
                                 case ConsoleKey.LeftArrow:
-                                    if (_board[i, j].WestField.Symbol != '#' && _board[i, j].WestField.Symbol != 'o' && _board[i, j].WestField.Symbol != '0')
-                                    {
-
-                                        _board[i, j].Object.Move(_board[i, j].WestField, _board[i, j], false);
-                                        MoveSleeper();
-                                        Console.Clear();
-                                        gameView.Board = this._board;
-                                        gameView.PrintObjects();
-                                        
-                                        return;
-                                    }
-                                    else if (_board[i, j].WestField.Symbol == 'o' || _board[i, j].WestField.Symbol == '0')
-                                    {
-
-
-                                        _board[i, j].WestField.Object.Move(_board[i, j].WestField.WestField, _board[i, j].WestField, false);
-                                        _board[i, j].Object.Move(_board[i, j].WestField, _board[i, j], false);
-                                        MoveSleeper();
-                                        Console.Clear();
-                                        gameView.Board = this._board;
-                                        gameView.PrintObjects();
-                                        return;
-
-                                    }
+                                    MoveWest(i, j);
                                     return;
                                 case ConsoleKey.RightArrow:
-                                    if (_board[i, j].EastField.Symbol != '#' && _board[i, j].EastField.Symbol != 'o' && _board[i, j].EastField.Symbol != '0')
-                                    {
-
-                                        _board[i, j].Object.Move(_board[i, j].EastField, _board[i, j], false);
-                                        MoveSleeper();
-                                        Console.Clear();
-                                        gameView.Board = this._board;
-                                        gameView.PrintObjects();
-                                        return;
-                                    }
-                                    else if (_board[i, j].EastField.Symbol == 'o' || _board[i, j].EastField.Symbol == '0')
-                                    {
-
-                                        _board[i, j].EastField.Object.Move(_board[i, j].EastField.EastField, _board[i, j].EastField, false);
-                                        _board[i, j].Object.Move(_board[i, j].EastField, _board[i, j], false);
-                                        MoveSleeper();
-                                        Console.Clear();
-                                        gameView.Board = this._board;
-                                        gameView.PrintObjects();
-                                        return;
-                                    }
+                                    MoveEast(i, j);
                                     return;
                                 case ConsoleKey.UpArrow:
-                                    if (_board[i, j].NorthField.Symbol != '#' && _board[i, j].NorthField.Symbol != 'o' && _board[i, j].NorthField.Symbol != '0')
-                                    {
-                                        _board[i, j].Object.Move(_board[i, j].NorthField, _board[i, j], false);
-                                        MoveSleeper();
-                                        Console.Clear();
-                                        gameView.Board = this._board;
-                                        gameView.PrintObjects();
-                                        return;
-                                    }
-                                    else if (_board[i, j].NorthField.Symbol == 'o' || _board[i, j].NorthField.Symbol == '0')
-                                    {
-
-                                        _board[i, j].NorthField.Object.Move(_board[i, j].NorthField.NorthField, _board[i, j].NorthField, false);
-                                        _board[i, j].Object.Move(_board[i, j].NorthField, _board[i, j], false);
-                                        MoveSleeper();
-                                        Console.Clear();
-                                        gameView.Board = this._board;
-                                        gameView.PrintObjects();
-                                        return;
-
-                                    }
+                                    MoveNorth(i, j);
                                     return;
                                 case ConsoleKey.DownArrow:
-                                    if (_board[i, j].SouthField.Symbol != '#' && _board[i, j].SouthField.Symbol != 'o' && _board[i, j].SouthField.Symbol != '0')
-                                    {
-                                        _board[i, j].Object.Move(_board[i, j].SouthField, _board[i, j], false);
-                                        MoveSleeper();
-                                        Console.Clear();
-                                        gameView.Board = this._board;
-                                        gameView.PrintObjects();
-                                        return;
-                                    }
-                                    else if (_board[i, j].SouthField.Symbol == 'o' || _board[i, j].SouthField.Symbol == '0')
-                                    {
-                                        _board[i, j].SouthField.Object.Move(_board[i, j].SouthField.SouthField, _board[i, j].SouthField, false);
-                                        _board[i, j].Object.Move(_board[i, j].SouthField, _board[i, j], false);
-                                        MoveSleeper();
-                                        Console.Clear();
-                                        gameView.Board = this._board;
-                                        gameView.PrintObjects();
-                                        return;
-                                    }
+                                    MoveSouth(i, j);
                                     return;
                                 case ConsoleKey.S:
-                                    Console.Clear();
-                                    _parser = new Parser(this);
-                                    _inputView.ShowMenu();
-                                    keyInputEvent();
+                                    goBackToMenu();
                                     return;
                                 case ConsoleKey.R:
-                                    Console.Clear();
-                                    _parser = new Parser(this);
-                                    _parser.ParseLevel(level);
+                                    resetLevel();
                                     return;
                             }
 
@@ -245,13 +250,36 @@ namespace ConsoleApp4
 
         }
 
+        public void goBackToMenu()
+        {
+            Console.Clear();
+            _parser = new Parser(this);
+            _inputView.ShowMenu();
+            keyInputEvent();
+
+        }
+
+        public void resetLevel()
+        {
+            _parser = new Parser(this);
+            _parser.ParseLevel(level);
+            _board = _parser.Object;
+            numOfRows = _parser.NumberOfRows;
+            lengthOfRows = _parser.LengthOfRows;
+            gameView = new GameView(_board, numOfRows, lengthOfRows);
+            Console.Clear();
+            gameView.PrintObjects();
+        }
+
+
+
         public void MoveSleeper()
         {
             for (int i = 0; i < numOfRows; i++)
             {
                 for (int j = 0; j < lengthOfRows; j++)
                 {
-                    if (_board[i, j].Symbol == '$' || _board[i, j].Symbol == 'z'  )
+                    if (_board[i, j].Symbol == '$' || _board[i, j].Symbol == 'z')
                     {
                         _board[i, j].Object.Move(_board[i, j], _board[i, j], false);
                         return;
@@ -283,4 +311,3 @@ namespace ConsoleApp4
 }
 
 
-        
